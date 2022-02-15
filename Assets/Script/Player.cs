623cd6c029f0c34d43add4f790d5d037ce09f235
kimhyunjin2628 <54582019+kimhyunjin2628@users.noreply.github.com>
2022-02-15 17:18:28 +0900

    create testPlayer

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10.0F;
+    public float rotationSpeed = 100.0F;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+    }
+}
