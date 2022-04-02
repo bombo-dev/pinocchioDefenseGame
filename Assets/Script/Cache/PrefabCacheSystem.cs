b95420cd527945e6ddb275bafbed72cd570ec07e
tkghlvoa <tkghlvoa@naver.com>
2022-04-02 17:40:16 +0900

    modify Script

--- Assets/Script/Cache/PrefabCacheSystem.cs
+++ Assets/Script/Cache/PrefabCacheSystem.cs
@@ -11,9 +11,17 @@ public class PrefabCacheData
 
 public class PrefabCacheSystem : MonoBehaviour
 {
+    //생성된 프리팹 정보
     Dictionary<string, Queue<GameObject>> prefabCaChes = new Dictionary<string, Queue<GameObject>>();
 
-    public void GeneratePrefabCache(string filePath, int cacheCount)
+    /// <summary>
+    /// 프리팹을 여러개 비활성 상태로 미리 생성해 놓고 정보를 딕셔너리에 저장 : 김현진
+    /// </summary>
+    /// <param name="filePath">프리팹 파일 주소</param>
+    /// <param name="cacheCount">생성할 프리팹 숫자</param>
+    /// <param name="gameObject">프리팹 게임 오브젝트</param>
+    /// <param name="parentsGameObject">생성될 게임 오브젝트의 부모 오브젝트</param>
+    public void GeneratePrefabCache(string filePath, int cacheCount, GameObject gameObject, Transform parentsGameObject = null)
     {
         //이미 같은 filePath의 캐시를 생성 한 경우
         if (prefabCaChes.ContainsKey(filePath))
@@ -22,7 +30,11 @@ public class PrefabCacheSystem : MonoBehaviour
         Queue<GameObject> queue = new Queue<GameObject>();
         for (int i = 0; i < cacheCount; i++)
         {
-            //queue.Enqueue 
+            GameObject go = Instantiate<GameObject>(gameObject, parentsGameObject);
+            go.SetActive(false);
+            queue.Enqueue(go);
+           // queue.Enqueue(Instantiate<GameObject>(gameObject, parentGameObject));
         }
+        prefabCaChes.Add(filePath, queue);
     }
 }
--- Assets/Script/Enemy/EnemyManager.cs
+++ Assets/Script/Enemy/EnemyManager.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
-    List<Enemy> enemises = new List<Enemy>();
+    //Load한 Enemy 프리팹 정보
+    Dictionary<string, GameObject> prefabCaChes = new Dictionary<string, GameObject>();
 
+    [SerializeField]
+    Transform enemyParents;
+
+    //filePath, cacheCount 저장
     [SerializeField]
     PrefabCacheData[] prefabCacheDatas;
 
@@ -15,14 +20,35 @@ public class EnemyManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrepareData();
     }
 
+    /// <summary>
+    /// 씬 로드 후 Enemy 캐시 데이터를 바탕으로 생성할 함수 호출 : 김현진
+    /// </summary>
     void PrepareData()
     {
         for (int i = 0; i < prefabCacheDatas.Length; i++)
         {
-            prefabCacheSystem.GeneratePrefabCache(prefabCacheDatas[i].filePath, prefabCacheDatas[i].cacheCount);          
+            prefabCacheSystem.GeneratePrefabCache(prefabCacheDatas[i].filePath, prefabCacheDatas[i].cacheCount, Load(prefabCacheDatas[i].filePath), enemyParents);          
+        }
+    }
+
+    /// <summary>
+    /// 프리팹 경로를 통해 게임오브젝트를 가져온다 : 김현진
+    /// </summary>
+    /// <param name="filePath">프리팹이 저장되있는 경로</param>
+    /// <returns>경로에서 가져온 게임 오브젝트</returns>
+    public GameObject Load(string filePath)
+    {
+        //이미 캐시에 포함되어 있을 경우
+        if (prefabCaChes.ContainsKey(filePath))
+            return prefabCaChes[filePath];
+        else
+        {
+            GameObject go = Resources.Load<GameObject>("Enemy/" + filePath);
+            prefabCaChes.Add(filePath, go);
+            return go;
         }
     }
 
