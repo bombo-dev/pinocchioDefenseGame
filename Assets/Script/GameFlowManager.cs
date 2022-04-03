1ab1dcda46d53356d1fc38a16b9abc489ccc9218
tkghlvoa <tkghlvoa@naver.com>
2022-04-03 20:48:41 +0900

    add GameFlowManagerScript

---
+++ Assets/Script/GameFlowManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFlowManager : MonoBehaviour
+{
+    //Enemy가 따라갈 타일
+    public int[] targetTileIndexArr1;
+    public int[] targetTileIndexArr2;
+    public int[] targetTileIndexArr3;
+
+    //Enemy가 생성되는 시간
+    public int[] timeFlowIndexArr1;
+    public int[] timeFlowIndexArr2;
+    public int[] timeFlowIndexArr3;
+
+    //생성되는 Enemy의 종류
+    public int[] enemyFlowIndexArr1;
+    public int[] enemyFlowIndexArr2;
+    public int[] enemyFlowIndexArr3;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
