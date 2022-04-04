770831df2fb52192a5fcbf721d9cd7d365211bee
kimhyunjin2628 <54582019+kimhyunjin2628@users.noreply.github.com>
2022-04-04 19:51:25 +0900

    modify LoadJsonScript

--- Assets/Script/LoadJson.cs
+++ Assets/Script/LoadJson.cs
@@ -1,8 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class LoadJson : MonoBehaviour
 {
+    /// <summary>
+    /// Json 파일로부터 Json 데이터 가져오기
+    /// </summary>
+    public void PrepareGameFlowJsonData()
+    {
+        //Json 불러오기
+        string filePath;
+        filePath = Path.Combine(Application.streamingAssetsPath, "Json");
 
+        /*
+        //자료구조 초기화
+        LocalizationData.Clear();
+
+        filePath += ".Json";
+        string JsonString = File.ReadAllText(filePath);
+
+        LocalizationDataList datas = JsonUtility.FromJson<LocalizationDataList>(JsonString);
+        for (int i = 0; i < datas.datas.Count; i++)
+        {
+            Debug.Log("/" + datas.datas[i].key + "/" + datas.datas[i].value);
+            LocalizationData.Add(datas.datas[i].key, datas.datas[i].value);
+        }
+        */
+    }
 }
