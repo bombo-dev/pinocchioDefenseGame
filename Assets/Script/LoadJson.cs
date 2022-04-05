c83fa214d87d50e70fa0e29d2f679892df91b89a
tkghlvoa <tkghlvoa@naver.com>
2022-04-06 00:50:00 +0900

    modify loadJson

--- Assets/Script/LoadJson.cs
+++ Assets/Script/LoadJson.cs
@@ -5,14 +5,10 @@ using System.IO;
 
 public class LoadJson : MonoBehaviour
 {
-    private void Start()
-    {
-        PrepareGameFlowJsonData();
-    }
     /// <summary>
     /// Json 파일로부터 Json 데이터 가져오기
     /// </summary>
-    public void PrepareGameFlowJsonData()
+    public DefenseFlowDataList PrepareGameFlowJsonData()
     {
         DefenseFlowData[] defenseFlowDatas = new DefenseFlowData[3];
 
@@ -27,12 +23,6 @@ public class LoadJson : MonoBehaviour
 
         DefenseFlowDataList datas = JsonUtility.FromJson<DefenseFlowDataList>(JsonString);
 
-        Debug.Log(datas.datas[0].defenseFlowDataArr[2].targetTileIndexArr[7]);
-        for (int i = 0; i < datas.datas.Count; i++)
-        {
-            Debug.Log(datas.datas[i]);
-        }
-        
-        
+        return datas;
     }
 }
