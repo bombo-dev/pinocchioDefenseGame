788fc8978b69b1f58d8eda450b0e3736032b84c8
Bombo <bombo96@chungwoon.ac.kr>
2022-05-18 00:40:01 +0900

    Modify originPath Variable in LoadJsonFile Method

--- Assets/Script/public/LoadJson.cs
+++ Assets/Script/public/LoadJson.cs
@@ -27,44 +27,17 @@ public class LoadJson : MonoBehaviour
         string filePath;
 
         //Json 경로 체크
-        filePath = PathCheck();
+        filePath = PathInit();
 
         return LoadJsonFile<DefenseFlowDataList>(filePath);
     }
 
-    //Json File PC, 모바일 경로 체크
-    public string PathCheck()
+    //Json File 초기화
+    public string PathInit()
     {
         string filePath;
-
-        if (Application.platform == RuntimePlatform.WindowsPlayer)
-        {
             filePath = Path.Combine(Application.streamingAssetsPath, "Test.Json");
             return filePath;
-        }
-
-        else
-        {
-            filePath = Path.Combine(Application.streamingAssetsPath, "Test.json");
-            return filePath;
-        }
-        /*
-        // PC 경로 체크
-        if (File.Exists(Path.Combine(Application.streamingAssetsPath, "Test.Json"))) {
-            
-            }
-        else
-            return Application.persistentDataPath + "/Test.Json";
-
-        // 모바일 경로 체크
-        
-            if (File.Exists(Application.persistentDataPath + "/Test.Json"))
-        {
-            filePath = Application.persistentDataPath + "/Test.Json";
-            return filePath;
-        }
-        */
-        
     }
 
     // JsonData의 객체화 메소드
@@ -75,63 +48,28 @@ public class LoadJson : MonoBehaviour
 
     // JsonData 불러오는 메소드
     public static DefenseFlowDataList LoadJsonFile<DefenseFlowDataList>(string filePath) {
+
+        // PC 경로 체크
         if (Application.platform == RuntimePlatform.WindowsPlayer)
         {
             string jsonString = File.ReadAllText(filePath);
             return JsonToObject<DefenseFlowDataList>(jsonString);
         }
+
+        // 모바일 경로 체크
         else
         {
             string originPath = Path.Combine(Application.streamingAssetsPath, "Test.Json");
+
             WWW reader = new WWW(originPath);
             while (!reader.isDone) { }
+
             string realPath = Application.persistentDataPath + ".Json";
             File.WriteAllBytes(realPath, reader.bytes);
+
             string jsonString = File.ReadAllText(realPath);
+
             return JsonToObject<DefenseFlowDataList>(jsonString);
         }
     }
-
-    /*
-    public static string Decrypt(string textToDecrypt, string key)
-
-    {
-
-        RijndaelManaged rijndaelCipher = new RijndaelManaged();
-
-        rijndaelCipher.Mode = CipherMode.CBC;
-
-        rijndaelCipher.Padding = PaddingMode.PKCS7;
-
-
-
-        rijndaelCipher.KeySize = 128;
-
-        rijndaelCipher.BlockSize = 128;
-
-        byte[] encryptedData = Convert.FromBase64String(textToDecrypt);
-
-        byte[] pwdBytes = Encoding.UTF8.GetBytes(key);
-
-        byte[] keyBytes = new byte[16];
-
-        int len = pwdBytes.Length;
-
-        if (len > keyBytes.Length)
-
-        {
-            len = keyBytes.Length;
-        }
-
-        Array.Copy(pwdBytes, keyBytes, len);
-
-        rijndaelCipher.Key = keyBytes;
-
-        rijndaelCipher.IV = keyBytes;
-
-        byte[] plainText = rijndaelCipher.CreateDecryptor().TransformFinalBlock(encryptedData, 0, encryptedData.Length);
-
-        return Encoding.UTF8.GetString(plainText);
-    }
-    */
 }
