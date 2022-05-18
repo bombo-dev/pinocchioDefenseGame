fc133ec83b70153b665423348a61a2ffe0a06646
Bombo <bombo96@chungwoon.ac.kr>
2022-05-18 11:10:04 +0900

    Add Encrypt, Decrypt Method

--- Assets/Script/public/LoadJson.cs
+++ Assets/Script/public/LoadJson.cs
@@ -72,4 +72,80 @@ public class LoadJson : MonoBehaviour
             return JsonToObject<DefenseFlowDataList>(jsonString);
         }
     }
+
+    // 암호화 메소드
+    public static string Decrypt(string textToDecrypt, string key)
+    {
+        RijndaelManaged rijndaelCipher = new RijndaelManaged();
+
+        rijndaelCipher.Mode = CipherMode.CBC;
+
+        rijndaelCipher.Padding = PaddingMode.PKCS7;
+
+        rijndaelCipher.KeySize = 128;
+
+        rijndaelCipher.BlockSize = 128;
+
+        byte[] encryptedData = Convert.FromBase64String(textToDecrypt);
+
+        byte[] pwdBytes = Encoding.UTF8.GetBytes(key);
+
+        byte[] keyBytes = new byte[16];
+
+        int len = pwdBytes.Length;
+
+        if (len > keyBytes.Length)
+        {
+            len = keyBytes.Length;
+        }
+
+        Array.Copy(pwdBytes, keyBytes, len);
+
+        rijndaelCipher.Key = keyBytes;
+
+        rijndaelCipher.IV = keyBytes;
+
+        byte[] plainText = rijndaelCipher.CreateDecryptor().TransformFinalBlock(encryptedData, 0, encryptedData.Length);
+
+        return Encoding.UTF8.GetString(plainText);
+    }
+
+    // 복호화 메소드
+    public static string Encrypt(string textToEncrypt, string key)
+    {
+        RijndaelManaged rijndaelCipher = new RijndaelManaged();
+
+        rijndaelCipher.Mode = CipherMode.CBC;
+
+        rijndaelCipher.Padding = PaddingMode.PKCS7;
+
+        rijndaelCipher.KeySize = 128;
+
+        rijndaelCipher.BlockSize = 128;
+
+        byte[] pwdBytes = Encoding.UTF8.GetBytes(key);
+
+        byte[] keyBytes = new byte[16];
+
+        int len = pwdBytes.Length;
+
+        if (len > keyBytes.Length)
+
+        {
+            len = keyBytes.Length;
+        }
+
+        Array.Copy(pwdBytes, keyBytes, len);
+
+        rijndaelCipher.Key = keyBytes;
+
+        rijndaelCipher.IV = keyBytes;
+
+        ICryptoTransform transform = rijndaelCipher.CreateEncryptor();
+
+        byte[] plainText = Encoding.UTF8.GetBytes(textToEncrypt);
+
+        return Convert.ToBase64String(transform.TransformFinalBlock(plainText, 0, plainText.Length));
+    }
+
 }
