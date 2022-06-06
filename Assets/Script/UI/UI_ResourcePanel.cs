e49d66cadfcf0bcdc0d2bec8e3372c8d922f009c
kimhyunjin2628 <54582019+kimhyunjin2628@users.noreply.github.com>
2022-06-06 12:34:10 +0900

    create script_ ResourceManager, ResourceManagerPanel

---
+++ Assets/Script/UI/UI_ResourcePanel.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_ResourcePanel : UI_Controller
+{
+    enum TextMeshProUGUIs
+    {
+        woodResourceText
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    void UpdateWoodResource()
+    {
+    }
+}
