db83ae9ab8690f7455ccf7712d216c2d1b0768e1
eunbi <ob1471@naver.com>
2022-06-17 22:04:49 +0900

    Add debuff remark

--- Assets/Script/UI/StatusMngPanel.cs
+++ Assets/Script/UI/StatusMngPanel.cs
@@ -92,71 +92,112 @@ public class StatusMngPanel : UI_Controller
         hp = currentHP;
     }
 
-
+    /// <summary>
+    /// 디버프 설정
+    /// </summary>
+    /// <param name="debuffIdx"></param>
+    /// <param name="debuffs"></param>
+    /// <param name="time"></param>
     public void SetDebuff(int debuffIdx, Dictionary<Actor.debuff, Debuff> debuffs , float time)
     {
+        // 예외처리
         if (debuffIdx <= 0 || hp <= 0)
             return;
 
         if (gameObject.activeSelf == false)
             return;
 
+        // 해당 인덱스의 디버프 가져오기
         GameObject go = Debuffs[debuffIdx - 1];
 
         if (!go)
             return;
 
+        // 디버프 텍스트 가져오기
         TextMeshProUGUI debuffText = go.GetComponentInChildren<TextMeshProUGUI>();
 
-        int stack = debuffs[(Actor.debuff)debuffIdx].stack;
-        
+        // 스택 정보 받아오기
+        int stack = debuffs[(Actor.debuff)debuffIdx].stack;        
 
+        // 디버프 게이지 오브젝트 가져오기
         Transform GoTransform = go.transform.GetChild(0).transform.GetChild(0);
         Image ImgFillAmount = GoTransform.GetComponent<Image>();
 
+        // 디버프의 경과시간을 카운트 할 변수 초기화
         debuffFlowTime = 0.0f;
 
+        // 디버프 중첩시
         if (stack >= 2)
         {
+            // 이전에 실행되던 코루틴 종료
             StopCoroutine(DebuffCoroutine(ImgFillAmount, time, debuffIdx));
+
+            // 중첩 정보를 화면에 표시
             debuffText.text = "X" + stack.ToString();
+
             Debug.Log("잔여 시간=" + debuffLeftTime);
+
+            // 디버프 지속시간에 디버프 잔여시간을 합해주기
             time += debuffLeftTime;
+
             Debug.Log("합산 시간=" + time);
         }
         else
         {
+            // 디버프 중첩 텍스트를 공백으로 표시
             debuffText.text = " ";
+
+            // 디버프 잔여 시간 초기화
             debuffLeftTime = 0.0f;
         }
 
-
-
+        // 디버프 UI 활성화
         go.SetActive(true);
 
+        // 코루틴 시작
         StartCoroutine(DebuffCoroutine(ImgFillAmount, time, debuffIdx));
-
     }
     
+    /// <summary>
+    /// 디버프 UI 제어를 위한 코루틴
+    /// </summary>
+    /// <param name="image">디버프 게이지를 표시할 이미지</param>
+    /// <param name="time">디버프 지속시간</param>
+    /// <param name="debuffIdx">디버프 인덱스</param>
+    /// <returns></returns>
     IEnumerator DebuffCoroutine(Image image, float time, int debuffIdx)
     {
         while (true)
         {
-
+            // 지속시간이 다됐을 때 or 패널이 비활성화 상태일 때
             if (debuffFlowTime >= time || gameObject.activeSelf == false)
             {
+                // 코루틴 종료
                 StopCoroutine(DebuffCoroutine(image, time, debuffIdx));
+                
                 Debug.Log("Dead---------");
+
+                // 디버프 패널 비활성화
                 RemoveDebuff(image, time, debuffIdx);
+                
                 //debuffFlowTime = 0.0f;
 
             }
             Debug.Log("time=" + time);
+
+            // 디버프 경과시간 카운트
             debuffFlowTime += Time.deltaTime;
+
+            // 디버프 경과시간을 게이지 UI로 표시
             image.fillAmount = (debuffFlowTime/time);
+
+            // 디버프 잔여시간 구하기
             debuffLeftTime = time - debuffFlowTime;
+
             Debug.Log("flowTime=" + debuffFlowTime);
+
             //Debug.Log("leftTime=" + debuffFlowTime);
+
             yield return new WaitForSeconds(Time.deltaTime);
                  
         }
