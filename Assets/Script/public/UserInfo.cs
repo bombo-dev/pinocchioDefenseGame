ea183bf5e3874a5d273bb9b5e5b3d7753c6bc567
Bombo <bombo96@chungwoon.ac.kr>
2022-06-21 22:03:29 +0900

    Add UserInfo Constructor

--- Assets/Script/public/UserInfo.cs
+++ Assets/Script/public/UserInfo.cs
@@ -7,6 +7,12 @@ public class StageStar
 {
     public int stageNum;    //스테이지
     public int starNum;  //클리어 별
+
+    public StageStar()
+    {
+        stageNum = 0;
+        starNum = 0;
+    }
 }
 public class UserInfo : MonoBehaviour
 {
@@ -17,11 +23,33 @@ public class UserInfo : MonoBehaviour
     public int maxStageNum; //최대 클리어한 스테이지
     public int selectedStageNum;   //선택한 스테이지
 
-    public StageStar[] stageStarArr;    //스테이지 클리어 별 정보
+    public List<StageStar> stageStarList;    //스테이지 클리어 별 정보
 
     [Header("TurretInfo")]
     public int maxTurretNum;    //최대 터렛 숫자
     public List<int> turretPreset; //선택된 터렛 리스트
+
+    public UserInfo()
+    {
+        colorWoodResource[0] = 5;
+        colorWoodResource[1] = 7;
+        colorWoodResource[2] = 10;
+        colorWoodResource[3] = 9;
+        colorWoodResource[4] = 3;
+        colorWoodResource[5] = 2;
+
+        maxStageNum = 3;
+        selectedStageNum = 7;
+
+        stageStarList = new List<StageStar>();
+        stageStarList.Add(new StageStar());
+
+        maxTurretNum = 4;
+
+        turretPreset = new List<int>();
+        turretPreset.Add(3);
+        turretPreset.Add(2);
+    }
 
     private void Awake()
     {
