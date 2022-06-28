8a642c7c696fea0cfe894a52bdde936a53ee68b4
tkghlvoa <tkghlvoa@naver.com>
2022-06-28 23:09:27 +0900

    stage 27

--- Assets/Script/public/RewardManager.cs
+++ Assets/Script/public/RewardManager.cs
@@ -110,6 +110,7 @@ public class RewardManager : MonoBehaviour
         turretReward.Add(21, 12);//강화 아기새
         turretReward.Add(23, 13);//강화 독수리
         turretReward.Add(25, 14);//초강화 비둘기
+        turretReward.Add(27, 15);//초강화 딱다구리
 
 
     }
