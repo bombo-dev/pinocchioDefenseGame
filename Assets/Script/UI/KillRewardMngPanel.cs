55fa8651a161f913e7216c8c41c8fb24a86df7e6
eunbi <ob1471@naver.com>
2022-06-30 21:21:33 +0900

    KillRewardPanel's AlpaValue BugFix

--- Assets/Script/UI/KillRewardMngPanel.cs
+++ Assets/Script/UI/KillRewardMngPanel.cs
@@ -1,7 +1,6 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 
 public class KillRewardMngPanel : MonoBehaviour
 {
@@ -18,40 +17,43 @@ public class KillRewardMngPanel : MonoBehaviour
 
     KillRewardMngPanel killRewardMngPanel;
 
+    Coroutine runningCoroutine = null;
+
     private void Update()
     {
-        if(gameObject.activeSelf == true)
+        if (gameObject.activeSelf == true)
             UpdatePanelPos();
 
-        if(Time.timeScale == 0)
+        if (Time.timeScale == 0)
         {
             int panelNum = SystemManager.Instance.PanelManager.rewardPanels.Count;
             int i = 0;
 
-            while(i < panelNum)
-            {                                
+            while (i < panelNum)
+            {
                 SystemManager.Instance.PanelManager.DisablePanel<KillRewardMngPanel>(SystemManager.Instance.PanelManager.rewardPanels[0]);
                 i++;
-            }            
+            }
         }
     }
     public void ShowReward(int value)
     {
         alpaValue = 1;
         TextMeshProUGUI reward = RewardText.GetComponentInChildren<TextMeshProUGUI>();
-        reward.text = "+"+ value.ToString();
+        reward.text = "+" + value.ToString();
 
+        //reward.color = new Color(reward.color.r, reward.color.g, reward.color.b, alpaValue);
         reward.alpha = alpaValue;
 
         RewardText.SetActive(true);
 
-        StartCoroutine(ShowRwdCoroutine(value));
+        runningCoroutine = StartCoroutine(ShowRwdCoroutine(value, reward));
 
     }
 
-    IEnumerator ShowRwdCoroutine(int value)
+    IEnumerator ShowRwdCoroutine(int value, TextMeshProUGUI reward)
     {
-        while(true)
+        while (true)
         {
             if (alpaValue < 0.1f)
             {
@@ -59,10 +61,10 @@ public class KillRewardMngPanel : MonoBehaviour
                 SystemManager.Instance.PanelManager.DisablePanel<KillRewardMngPanel>(gameObject);
                 addPos = 0.1f;
 
-                StopCoroutine(ShowRwdCoroutine(value));
+                StopCoroutine(runningCoroutine);
             }
             alpaValue -= 0.03f;
-
+            reward.alpha = alpaValue;
             yield return new WaitForSeconds(0.01f);
 
         }
@@ -71,7 +73,7 @@ public class KillRewardMngPanel : MonoBehaviour
     void UpdatePanelPos()
     {
         Enemy enemy = rewardOwner.GetComponent<Enemy>();
-        transform.position = Camera.main.WorldToScreenPoint(new Vector3(enemy.hpPos.transform.position.x, enemy.hpPos.transform.position.y+addPos, enemy.hpPos.transform.position.z));
+        transform.position = Camera.main.WorldToScreenPoint(new Vector3(enemy.hpPos.transform.position.x, enemy.hpPos.transform.position.y + addPos, enemy.hpPos.transform.position.z));
         addPos += 0.1f;
     }
 }
